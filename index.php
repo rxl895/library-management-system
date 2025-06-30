1c027adb456a4e1ff429ab8a061e1f6cd4e23360
Ritika Lamba <143743393+rxl895@users.noreply.github.com>
2025-06-30 14:07:19 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Library Management System</title>
+</head>
+<body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
+  <h2>📚 Welcome to the Library</h2>
+  <a href="view_books.php">📖 View Books</a><br><br>
+  <a href="add_book.php">➕ Add New Book</a><br><br>
+  <a href="issue_book.php">📤 Issue Book</a><br><br>
+  <a href="return_book.php">📥 Return Book</a><br><br>
+  <a href="admin.php">🔐 Admin Dashboard</a>
+</body>
+</html>
